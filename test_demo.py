75f11d11357382cf86eff428cbbac91be7963227
no89757 <645323050@qq.com>
2020-12-09 20:53:54 +0800

    a new case

--- test_demo.py
+++ test_demo.py
@@ -1,23 +1,11 @@
 import pytest
 
 
-class TestDemo:
+def add_function(a, b):
+    return a + b
 
-    # 加法
-    @pytest.mark.parametrize("a, b, expected", [(1, 2, 3), (2, 3, 5), (3, 4, 8)])
-    def test_add(self, a, b, expected):
-        # 求和
-        sum = a + b
-        # 断言
-        assert sum == expected
 
-        # 减法
-
-    @pytest.mark.parametrize("a, b, expected", [(1, 2, -1), (8, 3, 5), (3, 4, 8)])
-    def test_sub(self, a, b, expected):
-        # 减法
-        s = a - b
-
-        # 断言
-
-        assert s == expected
+@pytest.mark.parametrize("a, b, expected", [
+    (3, 5, 8), (-1, -2, -3), (1000, 1000, 2000)])
+def test_add(a, b, expected):
+    assert add_function(a, b) == expected
